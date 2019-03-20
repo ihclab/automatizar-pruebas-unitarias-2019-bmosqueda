25ed28dd3cf223fe8cbd4e550169339216238ba2
Brandon Mosqueda <bmosqueda@ucol.mx>
2019-03-20 10:35:02 -0600

    Problema 3: procesar los campos individuales del caso de prueba, agregado en run-pruebas.js

--- run-pruebas.js
+++ run-pruebas.js
@@ -7,8 +7,30 @@ fs.readFile('./CasosPrueba.txt', 'utf8', (error, data) => {
   }
 
   let tests = data.split('\r\n');
+  let testsArr = [];
 
-  tests.forEach(function(elem) {
-    console.log(elem);
+  tests.forEach(function(test) {
+    let testInfo = test.split(':');
+
+    if(testInfo.length !== 4) {
+      //TODO: throw error
+      console.log('Caso de prueba con formato incorrecto');
+    }
+
+    let info = {
+      testId: testInfo[0],
+      methodName: testInfo[1],
+      params: [],
+      correctResult: testInfo[3]
+    };
+
+    let params = testInfo[2].split(' ');
+    params.forEach(function(param) {
+      info.params.push(param);
+    });
+
+    testsArr.push(info);
   }); 
+
+  console.log(testsArr);
 });
