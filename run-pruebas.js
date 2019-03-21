6a24f793b28c302931bd8fe35199ceadcb8837f6
Brandon Mosqueda <bmosqueda@ucol.mx>
2019-03-21 08:34:19 -0600

    Actividad 01: Registro de resultados. Al finalizar las pruebas los resultados los imprime en la consola con colores adecuados

--- run-pruebas.js
+++ run-pruebas.js
@@ -1,9 +1,57 @@
+//Para imprimir con colores en la consola
+const chalk = require('chalk');
 const stringToValidType = require('./string-to-valid-type');
 const round = require('./round');
 const fs = require('fs');
 const Medias = require('./medias.js');
 //Para el método que no es estático
 const medias = new Medias();
+
+function getSpaces(num) {
+  let spaces = '';
+  for (var i = 0; i < num; i++)
+    spaces += ' ';
+
+  return spaces;
+} 
+
+function saveTestResult(tests) {
+  let successNum = failuresNum = 0;
+
+  tests.forEach(function(test) {
+    let tmpStr = test.testId;
+    let tmpStrConsole = test.testId;
+    if(test.status < 0)
+      tmpStr += '               ';
+    else {
+      tmpStr += test.status === 1 ? '    Éxito      ' : '   *Falla*     ';
+    }
+    tmpStr += test.methodName + getSpaces(21 - test.methodName.length) ;
+
+    if(test.status < 0) {
+      tmpStr += test.result;
+    } else if(test.status == 1) {
+      successNum++;
+      tmpStr += 'Calculado = ' + test.result + getSpaces(12 - test.result.toString().length);
+      
+    } else {
+      failuresNum++;
+      tmpStr += 'Calculado = ' + test.result + getSpaces(12 - test.result.toString().length);
+      tmpStr += 'Esperado = ' + test.correctResult + getSpaces(12 - test.correctResult.toString().length);
+      
+    }
+
+    if(test.status < 0)
+      console.log(tmpStr);
+    else if(test.status == 1)
+      console.log(chalk.green(tmpStr));
+    else
+      console.log(chalk.red(tmpStr));   
+  });
+
+  console.log('\n\n=========================== Fin de la prueba ======================================\n');
+  console.log(chalk.green('                 Éxito = ' + successNum ), chalk.red('               Falla = ' + failuresNum + '\n\n'));
+}
 
 fs.readFile('./CasosPrueba.txt', 'utf8', (error, data) => {
   if(error) {
@@ -43,21 +91,25 @@ fs.readFile('./CasosPrueba.txt', 'utf8', (error, data) => {
     tests.push(info);
 
     try {
-      //Evaluar cada caso mandando llamar al método
-      if(typeof Medias[info.methodName] === 'function') { //Métodos estáticos
-        info.result = Medias[info.methodName](info.params);
-      } else if(typeof medias[info.methodName] === 'function') { //Métodos propios de la instancia
-        info.result = medias[info.methodName](info.params);
-      } else {
-        info.result = info.methodName + ' Método no existente'
-      }
+      //Métodos estáticos
+      let fn = Medias[info.methodName];
+
+      if(typeof fn !== 'function') //Métodos de la instancia
+        fn = medias[info.methodName];
+
+      if(typeof fn !== 'function')
+        throw 'Método no existente';
+
+      
+      info.result = fn(info.params);
+      
+      info.status = info.result === info.correctResult ? 1 : 0;
     } catch(error) {
-      //Validación para los métodos que generan una excepción
+      info.status = -1; //Excepción
+      //Validación para los métodos que generan una excepción o cuando el método no existe
       info.result = error;
     }
-
-    info.isSuccess = info.result === info.correctResult;
   }); 
 
-  console.log(tests);
+  saveTestResult(tests);
 });
